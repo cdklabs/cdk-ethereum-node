698690997262c0b95f54b64a5c90ddc596855f36
trinikey <trinikey@amazon.com>
2022-08-22 15:54:51 -0400

    Move complete implementation examples to a subfolder and clear out attempts to integrate them with the main project

---
+++ examples/python/ethereum.py
@@ -0,0 +1,25 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+import os
+import aws_cdk as cdk
+import cdk_ethereum_node as ethereum
+
+class EthereumCdkStack(cdk.Stack):
+    def __init__(self, scope, id, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        # Default Ethereum node deployment onto Amazon Managed Blockchain
+        # Network - Ethereum Ropsten
+        # Availability Zone - 'us-east-1a'
+        # Instance Type - 'bc.t3.large'
+
+        ethereum_node = ethereum.EthereumNode(self, "MyDefaultEthereumNode")
+
+        # Custom network configurations for Ethereum node delpoyments
+        
+        custom_ethereum_node = ethereum.EthereumNode(self, "MyCustomEthereumNode",
+            network_id = ethereum.EthereumNode.NetworkId.MAINNET,
+            availability_zone = 'us-east-1a',
+            instance_type = 'bc.t3.large'
+        );
